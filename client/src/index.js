a2a6cdff0aff443630c75c0427d2ab2579a19215
Youngjae Kim <2015fromjeki@gmail.com>
2024-03-21 19:28:19 +1100

    Update
    Update

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <p>Hello World</p>
     <App />
   </React.StrictMode>
 );
